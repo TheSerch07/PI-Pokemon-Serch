e7c9b744ca2893ddb5317306c917b0aaccf5c4b4
TheSerch07 <jejog50@gmail.com>
2022-05-11 17:35:34 -0500

    frontend

--- client/src/components/Order.jsx
+++ client/src/components/Order.jsx
@@ -1,5 +1,8 @@
 export default function Order() {
-    return <select>
+    function actualSelect(e) {
+        console.log(e.target.value)
+    }
+    return <select onChange={actualSelect}>
         <option value="Ascendente">A-Z</option>
         <option value="Descendente">Z-A</option>
     </select>
